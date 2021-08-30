70e5c3942615aa113d7ac5eebc54adbd0daae61e
Kalasouski <40716481+Kalasouski@users.noreply.github.com>
2021-08-30 21:36:26 +0300

    initial commit

---
+++ src/playground/test.js
@@ -0,0 +1,16 @@
+const fetch = require("node-fetch");
+
+////
+
+
+/*fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {response.json()}).then(console.log).catch(reject=> {
+        console.log("dgy")
+}*/
+
+
+
+
+let t = {name : "val"};
+
+console.log(t.children)
